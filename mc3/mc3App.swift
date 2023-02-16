e0b46b5f0fc392b4026dd8b603df5a8967dec2df
alevinaccia <vinacciaalessandro@gmail.com>
2023-02-16 12:11:56 +0100

    first

---
+++ mc3/mc3App.swift
@@ -0,0 +1,17 @@
+//
+//  mc3App.swift
+//  mc3
+//
+//  Created by Alessandro Vinaccia on 16/02/23.
+//
+
+import SwiftUI
+
+@main
+struct mc3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
