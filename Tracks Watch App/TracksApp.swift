afbe175c227bff43aed567189ee3ad1965ab9f32
acacciola22 <acacciola22@fed.idserve.net>
2023-02-27 09:30:29 +0100

    watchOS

---
+++ Tracks Watch App/TracksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TracksApp.swift
+//  Tracks Watch App
+//
+//  Created by antonia on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Tracks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
